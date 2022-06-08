c86820f7125e62303d83ec689a3e3d76e70b190b
ElisaTorelli <elisa.torelli1@gmail.com>
2022-06-08 16:04:23 +0200

    add code snippet

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 import { getApiKey } from "./env.js";
 
 let apiKey = getApiKey();
-console.log(apiKey);
+
+
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&quot`)
+.then(res => res.json())
+.then((data) => {
+    results
+    
+})
