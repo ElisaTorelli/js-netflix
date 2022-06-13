1b208662aee6c95b4d223bad3b7a9c1b42081f38
ElisaTorelli <elisa.torelli1@gmail.com>
2022-06-13 17:04:48 +0200

    add film rows

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ let leftArrow = document.querySelector(".left");
 let rightArrow = document.querySelector(".right");
 let container = document.querySelector(".container");
 let row = document.querySelectorAll(".row");
-let posterContainer = document.querySelector(".row-poster-container")
+let posterContainer = document.querySelector(".row-poster-container");
+let numberContainer = document.querySelector(".container-movie");
 
 const getMovies = async()=>{
     let resultApi = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&quot`)
@@ -26,7 +27,6 @@ cardsContainer.forEach((element)=>{
 
 function displayMovies(container, res){
     res.forEach(element=>{
-        
         container.innerHTML +=
         `
         <div class="general-container">
@@ -93,15 +93,66 @@ function scrollEffect(){
 scrollEffect()
 
 
+
+// getMovies().then(res => {
+//     res.results.forEach(element=>{
+//         posterContainer.innerHTML += `
+//         <div class="card-container">
+//             <div class="card-poster">
+//                 <img class="poster-img" src="https://image.tmdb.org/t/p/original${element.backdrop_path}" alt="poster1" />
+//             </div>
+//         </div>
+//         `
+//     })
+// });
+
+
 // ROW POSTER
-getMovies().then(res => {
-    res.results.forEach(element=>{
-        posterContainer.innerHTML += `
-        <div class="card-container">
-            <div class="card-poster">
-                <img class="poster-img" src="https://image.tmdb.org/t/p/original${element.backdrop_path}" alt="poster1" />
-            </div>
+let posterArray = ['./assets/images/poster1.png', './assets/images/poster2.png', './assets/images/poster3.png', './assets/images/poster4.png'];
+
+function displayPosterImages (){
+    for(let i = 0; i < 5; i++){
+        for(let j = 0; j < posterArray.length; j++){
+            posterContainer.innerHTML += 
+            `
+                <div class="card-container">
+                    <div class="card-poster">
+                        <img class="poster-img" src="${posterArray[j]}" alt="poster" />
+                    </div>
+                </div>
+            `
+        }
+    }
+}
+displayPosterImages ();
+
+
+// CARD NUMBER
+// getMovies().then(res => {
+//     res.results.map((element, i)=>{
+//         numberContainer.innerHTML +=
+//         `
+//         <span class="number-movie">${i + 1}</span>
+//         <div class="movie">
+//             <img class="img-number" src="https://image.tmdb.org/t/p/original${element.backdrop_path}" alt="movie1">
+//         </div>
+//         `
+//     })
+// });
+
+
+// CARD NUMBER MOVIE LIST
+let numberArray = ['./assets/images/Movie1.png', './assets/images/Movie2.png', './assets/images/Movie3.png', './assets/images/Movie4.png', './assets/images/Movie1.png', './assets/images/Movie2.png', './assets/images/Movie3.png', './assets/images/Movie4.png', './assets/images/Movie1.png', './assets/images/Movie2.png'];
+
+function numberListMovie () {
+    for(let j = 0; j < numberArray.length; j++){
+        numberContainer.innerHTML +=
+        `
+        <span class="number-movie">${j + 1}</span>
+        <div class="movie">
+            <img class="img-number" src="${numberArray[j]}" alt="movie1">
         </div>
         `
-    })
-});
+    }
+}
+numberListMovie ();
